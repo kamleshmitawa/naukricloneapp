045122d8cfe0402e1cbcc45dca51ed2f88520e0a
Kamlesh <kmitawa@q3tech.com>
2021-10-03 15:21:29 +0530

    home path change

--- src/routes/routePath.js
+++ src/routes/routePath.js
@@ -1,11 +1,11 @@
 export const routePath = {
     root: '/',
+    home: '/home',
     login: '/login',
     signUp: '/signup',
     forgot: '/forget-password',
     reset: '/reset',
     logout: '/logout',
-    home: '/home',
     jobsPosted: '/jobs/posted',
     jobPost: '/job/post',
 }
