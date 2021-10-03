3dd2d8d44fc797db8e8bc46a007a1c9689496ee6
Kamlesh <kmitawa@q3tech.com>
2021-10-04 00:40:53 +0530

    applicatin added

--- src/components/Jobs/PostedJobs.jsx
+++ src/components/Jobs/PostedJobs.jsx
@@ -9,6 +9,9 @@ import { GET_POSTED_JOB } from "../../store/types/jobs.type";
 import { routePath } from "../../routes/routePath";
 import { Loader } from "../common/Loader";
 
+import "./job.css";
+import { ViewApplicantModal } from "./ViewApplicantModal";
+
 export const PostedJobs = () => {
   const [jobsList, setJobsList] = useState([]);
 
@@ -36,127 +39,37 @@ export const PostedJobs = () => {
   }
   return (
     <>
-      {/* <div class="bannerSection"> */}
-      <div class="container">
+      <div class="container job-container">
         <div class="row">
           <div class="col-lg-5">
             <h3>Jobs posted by you</h3>
           </div>
-        </div>
-      </div>
-      {/* </div> */}
-      <div class="container">
-        <div class="applicantsJobPopUp">
-          <div class="applicantsJob">
-            <div class="heading">
-              <div>Applicants for this job</div>
-              <span class="close"></span>
-            </div>
-            <div class="totalApplicants">Total 35 applications</div>
-            <div class="allApplications">
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
+          <div class="row postedJobs">
+            <div class="col-3 job">
+              <div class="job-title">UI UX Designer</div>
+
+              <div class="job-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt…
               </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
+              <div className="row job_bottom">
+                <div class="location">Bengaluru</div>
+                <div className="view-applicant">
+                  <button type="button">View applications</button>
                 </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-              <div class="application">
-                <div class="applicantName">
-                  <span class="letter">E</span>
-                  <label for="">Eliza Lucas</label>
-                  <div class="email">abc123@email.com</div>
-                </div>
-                <div class="">Skills</div>
-                <div class="skills">
-                  Coding, designing, graphics, website, app ui
-                </div>
-              </div>
-            </div>
-            <div class="noApplication">
-              <div>
-                <img src="#" alt="noaplicantion" />
-                <p>No applications available!</p>
               </div>
             </div>
           </div>
         </div>
+       <ViewApplicantModal />
         <div className="no-jobs">
           <p>Your posted jobs will show here!</p>
-          <Button
-            title="Post a Job"
-            onClickHandler={() => history.push(routePath.jobPost)}
-          />
+          <div className="row post-btn">
+            <Button
+              title="Post a Job"
+              onClickHandler={() => history.push(routePath.jobPost)}
+            />
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/Jobs/ViewApplicantModal.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import '../Home/home.css'
+
+export const ViewApplicantModal = () => {
+  return (
+    <div class="applicantsJobPopUp active">
+      <div class="applicantsJob">
+        <div class="heading">
+          <div>Applicants for this job</div>
+          <span class="close"></span>
+        </div>
+        <div class="totalApplicants">Total 35 applications</div>
+        <div class="allApplications">
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+          <div class="application">
+            <div class="applicantName">
+              <span class="letter">E</span>
+              <label for="">Eliza Lucas</label>
+              <div class="email">abc123@email.com</div>
+            </div>
+            <div class="">Skills</div>
+            <div class="skills">
+              Coding, designing, graphics, website, app ui
+            </div>
+          </div>
+        </div>
+        <div class="noApplication">
+          <div>
+            <img src="#" alt="noaplicantion" />
+            <p>No applications available!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
